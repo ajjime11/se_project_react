f1c42d417f12d55e5ba5d11c4eee02fe9f4b1a08
Alejandro Jimenez <ajjime11@asu.edu>
2025-08-25 09:21:50 -0700

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { useState } from "react";
+import "../../index.css";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>;
+    </>
+  );
+}
+
+export default App;
