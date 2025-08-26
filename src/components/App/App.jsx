e25e612b72ce2f4c62b2b9abffda1eacaee36ae3
Alejandro Jimenez <ajjime11@asu.edu>
2025-08-26 09:20:56 -0700

    Add WeatherCard component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,10 +6,13 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import WeatherCard from "../Main/WeatherCard/WeatherCard";
+
 function App() {
   return (
     <div className="app">
       <Header />
+      <WeatherCard />
       <Main />
       <Footer />
     </div>
