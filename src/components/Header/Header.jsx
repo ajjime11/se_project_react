1e380d748b56bf2c61957139307de6a61c24dd61
Alejandro Jimenez <ajjime11@asu.edu>
2025-09-05 02:55:15 -0700

    ToggleSwitch markup and functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../../assets/wtwr.png";
 import avatar from "../../assets/avatar.png";
 import "./Header.css";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 const Header = ({ onAddClick, city }) => {
   const now = new Date();
@@ -12,23 +13,28 @@ const Header = ({ onAddClick, city }) => {
 
   return (
     <header className="header">
-      <img src={logo} alt="" className="header__logo" />
-      <p className="header__place">
-        <time className="header__datetime" dateTime={now}>
-          {dateStr},
-        </time>{" "}
-        {city}
-      </p>
-      <div className="header__info-group">
-        <button onClick={onAddClick} className="header__add-clothes-btn">
-          + Add clothes
-        </button>
-        <p className="header__username">Terrence Tegegne</p>
-        <img
-          src={avatar}
-          alt="Terrence Tegegne's avatar"
-          className="header__avatar"
-        />
+      <div className="header__side">
+        <img src={logo} alt="" className="header__logo" />
+        <p className="header__place">
+          <time className="header__datetime" dateTime={now}>
+            {dateStr},
+          </time>{" "}
+          {city}
+        </p>
+      </div>
+      <div className="header__side">
+        <ToggleSwitch />
+        <div className="header__info-group">
+          <button onClick={onAddClick} className="header__add-clothes-btn">
+            + Add clothes
+          </button>
+          <p className="header__username">Terrence Tegegne</p>
+          <img
+            src={avatar}
+            alt="Terrence Tegegne's avatar"
+            className="header__avatar"
+          />
+        </div>
       </div>
     </header>
   );
